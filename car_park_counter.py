a8d979aa0c9aef17a7551c249a1e79bce3d7b253
Devan Anjelito <anjelitodevan@gmail.com>
2023-07-25 19:39:32 +0700

    Finish train test split.

--- car_park_counter.py
+++ car_park_counter.py
@@ -1,1 +1,34 @@
-    
+import os
+import utils
+from pathlib import Path
+from skimage.io import imread
+from skimage.transform import resize
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+# get images and labels
+image_dir= Path('dataset/parking lot/image')
+images, labels= utils.load_image_dataset(image_dir, shuffle= False)
+
+# convert images from path to images
+images= [imread(i) for i in images]
+
+# resize image to 15x15
+images= [resize(i, (15, 15)) for i in images]
+
+# flatten image to 1-d array
+# convert labels to array
+images= np.asarray([i.flatten() for i in images])
+labels= np.asarray(labels)
+
+# train test split
+x_train, x_test, y_train, y_test = train_test_split(
+    images, 
+    labels,
+    stratify= labels,
+    test_size= 0.2,
+    shuffle= True,  
+    random_state= 42
+)
+
+print(images.shape, labels.shape)
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import os
 
 def get_color_limits(color):
 
@@ -13,3 +14,32 @@ def get_color_limits(color):
     upper_limit= np.array(upper_limit, dtype= np.uint8)
 
     return lower_limit, upper_limit
+
+def load_image_dataset(dataset_dir, shuffle= False):
+
+    images_paths= []
+    labels= []
+
+    # iterate through each image folder
+    for c in os.listdir(dataset_dir):
+        c_dir = dataset_dir / c
+        # if not a folder, continue
+        if os.path.isfile(c_dir):
+            continue
+        # list all images inside image folder
+        # append to list
+        images= os.listdir(c_dir)
+        for image in images:
+            image_path= c_dir / image 
+            images_paths.append(image_path)
+            labels.append(c)
+
+    # shuffle dataset
+    if shuffle:
+        import random
+        random.seed(0)
+        indices = list(zip(images_paths, labels))
+        random.shuffle(indices)
+        images_paths, labels = zip(*indices)
+
+    return images_paths, labels
