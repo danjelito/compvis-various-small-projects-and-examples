04575d003a911c66ff7b18c13172841ce9ffab9a
Devan Anjelito <anjelitodevan@gmail.com>
2023-07-26 22:24:12 +0700

    Create detect_face function.

--- face_anonymizer.py
+++ face_anonymizer.py
@@ -1,25 +1,29 @@
 import cv2
 import matplotlib.pyplot as plt
 
+def detect_face(img, face_detection_object):
+
+    # convert image to greyscale
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # detect face -> will return x, y, w, h
+    face = face_classifier.detectMultiScale(
+        image=img_gray, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
+    )
+    (x, y, w, h) = face[0]
+    return x, y, w, h
+
 # read image
 img_path = "dataset/celeb faces/Brad Pitt/001_c04300ef.jpg"
-# img_path= 'dataset/parking lot/image/empty/00000000_00000161.jpg'
 img = cv2.imread(img_path)
-
-# convert image to greyscale
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # create face classifier
 face_classifier = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 )
 
-# detect face -> will return x, y, w, h
-face = face_classifier.detectMultiScale(
-    image=img_gray, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
-)
-print(face)
-(x, y, w, h) = face[0]
+# detect face
+x, y, w, h= detect_face(img, face_classifier)
 
 # plot face with bbox
 plot = False
@@ -38,5 +42,3 @@ img[y : y + h, x : x + w] = cv2.blur(
 )
 cv2.imshow("image", img)
 cv2.waitKey(0)
-
-# save image
