010faa49583c2626d853c3f48493ab53b697bd73
Devan Anjelito <anjelitodevan@gmail.com>
2023-07-27 17:53:48 +0700

    Separate blur and plot.

--- face_anonymizer.py
+++ face_anonymizer.py
@@ -18,19 +18,22 @@ def detect_face(img, face_detection_object):
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # detect face -> will return x, y, w, h
-    face = face_classifier.detectMultiScale(
+    face = face_detection_object.detectMultiScale(
         image=img_gray, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
     )
     (x, y, w, h) = face[0]
     return x, y, w, h
 
-def plot_blurred_face(img, x, y, w, h):
-
-    # blur face
+def blur_face(img, x, y, w, h):
+    
     img[y : y + h, x : x + w] = cv2.blur(
         src=img[y : y + h, x : x + w],
         ksize=(50, 50),
     )
+    return img
+
+def plot_blurred_face(img):
+    
     cv2.imshow('press any key to exit', img)
     cv2.waitKey(0)  
 
@@ -45,8 +48,23 @@ if args.mode in ['image']:
     # read image
     img_path = args.file_path
     img = cv2.imread(img_path)
+    # get face bbox
     x, y, w, h= detect_face(img, face_classifier)
-    plot_blurred_face(img, x, y, w, h)
+    # blur face
+    img= blur_face(img, x, y, w, h)
+    # display
+    plot_blurred_face(img)
+
+# elif args.mode in ['video']:
+
+#     # read video
+#     cap= cv2.VideoCapture(args.file_path)
+#     ret, frame= cap.read()
+#     while True:
+#         x, y, w, h= detect_face(img, face_classifier)
+        
+#         ret, frame= cap.read()
+#     cap.release()
 
 else:
     print('error: mode not recognized')
